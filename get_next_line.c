a70817995e6f3ff754ab71fda420cdc88902096c
Nicholas Saraiva Arruda Serafim <nsaraiva@c1r6s2.42porto.com>
2025-05-14 18:42:41 +0100

    new changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: nsaraiva <nsaraiva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 19:28:31 by nsaraiva          #+#    #+#             */
-/*   Updated: 2025/05/14 12:27:27 by nsaraiva         ###   ########.fr       */
+/*   Updated: 2025/05/14 18:42:29 by nsaraiva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	static t_list	*node;
+	static t_list	**node;
 	t_list	*new_node;
 	char	*buff;
 	int		a;
@@ -21,26 +21,26 @@ char	*get_next_line(int fd)
 	
 	lenght = 0;
 	a = 0;
-	if (node)
-		lenght += ft_strlen(node -> content);
-	if (fd < 0 || fd > 3)
-		return (0); 
+	if (*node)
+		lenght += ft_strlen((*node) -> content);
+	if (fd < 0)
+		return (0);
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return(0);
-	if (node && check_nl(node -> content))
-		return (returned_line(&node, BUFFER_SIZE));
-	while ((a = read(fd, buff, BUFFER_SIZE)))
+	if ((*node) && check_nl((*node) -> content))
+		return (returned_line(node, BUFFER_SIZE));
+	while ((a = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		buff[a] = '\0';
 		new_node = ft_lstnew(ft_strdup(buff));
 		if (!new_node)
 		{
-			ft_lstclear(&node, free);
+			ft_lstclear(node, free);
 			free(buff);
 			return (0);
 		}
-		ft_lstadd_back(&node, new_node);
+		ft_lstadd_back(node, new_node);
 		lenght += a;
 		if(check_nl(buff))
 			break;
@@ -49,15 +49,13 @@ char	*get_next_line(int fd)
 	return (returned_line(&node, lenght));
 }
 
-
-/*int	main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	int fd = open(argv[1], O_RDONLY);
 	char *buff;
 	if (argc)
 	while ((buff = get_next_line(fd)))
 	{
-		printf("%s", buff);
 		free(buff);
 	}
-}*/
+}
