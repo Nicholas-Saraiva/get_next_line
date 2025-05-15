4962241f060e94aa2e946e2695b56d0430268beb
CafeComLag <nicholas.saraivadev@gmail.com>
2025-05-15 11:50:15 +0100

    fixes on the new solution

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nsaraiva <nsaraiva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 11:29:41 by nsaraiva          #+#    #+#             */
-/*   Updated: 2025/05/14 22:55:49 by nsaraiva         ###   ########.fr       */
+/*   Updated: 2025/05/15 11:28:53 by nsaraiva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,34 @@
 int	ft_realoc(char *str1, char *str2)
 {
 	int		i;
+	int		size;
 	char	*new_str;
 
 	i = 0;
+	size = ft_strlen(str1) + ft_strlen(str2);
 	new_str = ft_strdup(str1);
-	if (*str1)
-		free(str1)
-	str1 = malloc((sizeof(char) * ft_strlen(str1) + ft_strlen(str2)) + 1);
-	if (!str1)
+	if (!new_str)
 		return (0);
-	while (*new_str)
+	free(str1);
+	str1 = malloc(sizeof(char) * (size + 1));
+	if (!str1)
 	{
-		str1[i++] = *new_str;
-		new_str++;
+		free(new_str);
+		return (0);
+	}
+	while (new_str[i])
+	{
+		str1[i] = new_str[i];
+		i++;
 	}
 	while (*str2)
 	{
-		str1[i++] = *str2;
-		str1++;
+		str1[i] = *str2;
+		i++;
+		str2++;
 	}
 	str1[i] = '\0';
+	free(new_str);
 	return (i);
 }
 
@@ -57,46 +65,42 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-char	*ft_newline(char , char *buff, int lenght)
+char	*ft_newline(char *str, char *buff)
 {	
-	char	*line;
 	int		i;
 	int		j;
 
 	i = 0;
 	j = 0;
-	line = malloc(sizeof(char) * lenght);
-	if (!line)
-		return (0);
 	while (buff[i]  && buff[i] != '\n')
-	{
-		line[i] = buff[i];
 		i++;
-	}
 	if (!buff[i])
-	{
-		free(buff);
-		return (line);
-	}
+		return (str);
+	str[i + 1] = '\0';
 	while (buff[i + j])
 		j++;
 	ft_memcpy(buff, buff + i, j);
-	return (line);
+	return (str);
 } 
 
 char	*get_next_line(int fd)
 {
-	static char	buff[BUFFER_SIZE];
+	static char	buff[BUFFER_SIZE + 1];
 	char	*str;
-	int		lenght;
+	int		num_bytes;
 
-	lenght = 0;
+	num_bytes = 0;
 	if (fd < 0)
 		return (0);
 	str = malloc (sizeof(char));
-	while (read(fd, buff, BUFFER_SIZE) > 0 && !check_nl(buff))
-		lenght = ft_realoc(str, buff);
-	return (ft_newline(str, buff, lenght));
+	while ((num_bytes = read(fd, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[num_bytes] = '\0';
+		ft_realoc(str, buff);
+		if (check_nl(buff))
+			break ;
+	}
+	return (ft_newline(str, buff));
 }
 
 int	main(int argc, char *argv[])
@@ -104,10 +108,6 @@ int	main(int argc, char *argv[])
 	int fd = open(argv[1], O_RDONLY);
 	char *buff;
 	if (argc)
-	{
 		while ((buff = get_next_line(fd)))
-		{
-			free(buff);
-		}
-	}
+			continue ;
 }
